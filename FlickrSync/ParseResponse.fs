7520ba25281e298e7f06c9001709a74701b252a7
Tom Riddelsdell <t.riddelsdell@gmail.com>
2015-05-06 18:58:27 +0100

    Added source code

---
+++ FlickrSync/ParseResponse.fs
@@ -0,0 +1,39 @@
+﻿module FlickrParser
+
+    open FSharp.Data
+ 
+    // Sample calendar data used by the type provider below for structure
+    [<Literal>]
+    let setsSample =
+        """<rsp stat="ok">
+            <photosets cancreate="1" page="1" pages="1" perpage="13" total="13">
+                <photoset id="72157651586794409" primary="17090490017" secret="c5abd40132" server="7701" farm="8" photos="37" videos="0" needs_interstitial="0" visibility_can_see_set="1" count_views="0" count_comments="0" can_comment="1" date_create="1429222948" date_update="1430205245">
+                    <title>Auto Sync</title>
+                    <description />
+                </photoset>
+                <photoset id="72157652014220202" primary="16987619190" secret="ae4f81c174" server="8771" farm="9" photos="241" videos="5" needs_interstitial="0" visibility_can_see_set="1" count_views="0" count_comments="0" can_comment="1" date_create="1429335697" date_update="1429600116">
+                    <title>2014-Cairngorms</title>
+                    <description />
+                </photoset>
+            </photosets>
+        </rsp>"""
+ 
+    [<Literal>]
+    let setSample =
+        """<rsp stat="ok">
+              <photoset id="72157652014220202" primary="16987619190" owner="131546405@N02" ownername="t.riddelsdell" page="1" per_page="500" perpage="500" pages="1" total="246" title="2014-Cairngorms">
+                <photo id="16987619190" secret="ae4f81c174" server="8771" farm="9" title="GoPro_20140216 125" isprimary="1" ispublic="0" isfriend="1" isfamily="1" />
+                <photo id="16987622380" secret="6e432dd9ff" server="7700" farm="8" title="GoPro_20140216 124" isprimary="0" ispublic="0" isfriend="1" isfamily="1" />
+              </photoset>
+            </rsp>"""
+ 
+    type setsDataProv = XmlProvider<setsSample>
+    type setDataProv = XmlProvider<setSample>
+ 
+    let ParseSets str =
+        let response = setsDataProv.Parse str
+        response.Photosets.Photosets
+ 
+    let ParseSet str =
+        let response = setDataProv.Parse str
+        response.Photoset.Photos
---
+++ FlickrSync/Program.fs
@@ -0,0 +1,193 @@
+﻿module FsTestApp
+
+open System
+open System.IO
+open System.Net
+open System.Security.Cryptography
+open System.Text
+open FSharp.Data
+open System.Collections.Generic
+ 
+// Twitter OAuth Constants
+let consumerKey = "8b2e1dfbe88471e9e5fdc7b558ce0b5d"
+let consumerSecret = "4433e410c1a894b3"
+let requestTokenURI = "http://www.flickr.com/services/oauth/request_token"
+let accessTokenURI = "https://www.flickr.com/services/oauth/access_token"
+let authorizeURI = "https://www.flickr.com/services/oauth/authorize"
+let apiMethodCallsURI = "https://api.flickr.com/services/rest"
+ 
+let nonce = System.Guid.NewGuid().ToString().Substring(24)
+ 
+// Utilities
+let unreservedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~";
+let urlEncode str =
+    String.init (String.length str) (fun i ->
+        let symbol = str.[i]
+        if unreservedChars.IndexOf(symbol) = -1 then
+            "%" + String.Format("{0:X2}", int symbol)
+        else
+            string symbol)
+ 
+ 
+// Core Algorithms
+let hmacsha1 signingKey str =
+    let converter = new HMACSHA1(Encoding.ASCII.GetBytes(signingKey : string))
+    let inBytes = Encoding.ASCII.GetBytes(str : string)
+    let outBytes = converter.ComputeHash(inBytes)
+    Convert.ToBase64String(outBytes)
+ 
+let compositeSigningKey consumerSecret tokenSecret =
+    urlEncode(consumerSecret) + "&" + urlEncode(tokenSecret)
+ 
+let baseString httpMethod baseUri queryParameters =
+    httpMethod + "&" +
+    urlEncode(baseUri) + "&" +
+    (queryParameters
+     |> Seq.sortBy (fun (k,v) -> k)
+     |> Seq.map (fun (k,v) -> urlEncode(k)+"%3D"+urlEncode(v))
+     |> String.concat "%26")
+ 
+let createAuthorizeHeader queryParameters =
+    let headerValue =
+        "OAuth " +
+        (queryParameters
+         |> Seq.map (fun (k,v) -> urlEncode(k)+"\x3D\""+urlEncode(v)+"\"")
+         |> String.concat ",")
+    headerValue
+ 
+let currentUnixTime = floor (DateTime.UtcNow - DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds
+ 
+ 
+/// Request a token from Twitter and return:
+///  oauth_token, oauth_token_secret, oauth_callback_confirmed
+let requestToken() =
+    let signingKey = compositeSigningKey consumerSecret ""
+ 
+    let queryParameters =
+        ["oauth_callback", "oob";
+         "oauth_consumer_key", consumerKey;
+         "oauth_nonce", nonce;
+         "oauth_signature_method", "HMAC-SHA1";
+         "oauth_timestamp", currentUnixTime.ToString();
+         "oauth_version", "1.0"]
+ 
+    let signingString = baseString "POST" requestTokenURI queryParameters
+    let oauth_signature = hmacsha1 signingKey signingString
+ 
+    let realQueryParameters = ("oauth_signature", oauth_signature)::queryParameters
+   
+    let resp = Http.RequestString(requestTokenURI, body = FormValues realQueryParameters, httpMethod = "POST")
+ 
+    seq { for mapping in resp.Split('&') do
+            let kv = mapping.Split('=')
+            yield kv.[0], kv.[1]}
+    |> Map.ofSeq
+ 
+/// Get an access token from Flikr and returns:
+///   oauth_token, oauth_token_secret
+let accessToken token tokenSecret verifier =
+    let signingKey = compositeSigningKey consumerSecret tokenSecret
+ 
+    let queryParameters =
+        [
+         "oauth_consumer_key", consumerKey;
+         "oauth_nonce", nonce;
+         "oauth_signature_method", "HMAC-SHA1";
+         "oauth_timestamp", currentUnixTime.ToString();
+         "oauth_token", token;
+         "oauth_verifier", verifier;
+         "oauth_version", "1.0"]
+ 
+    let signingString = baseString "POST" accessTokenURI queryParameters
+    let oauth_signature = hmacsha1 signingKey signingString
+   
+    let realQueryParameters = ("oauth_signature", oauth_signature)::queryParameters
+   
+    let headerValue = createAuthorizeHeader realQueryParameters
+   
+    let resp = Http.RequestString(  accessTokenURI ,
+                                    //headers = [ HttpRequestHeader.Authorization.ToString(), headerValue ],
+                                    body = FormValues realQueryParameters,
+                                    httpMethod = "POST")
+   
+    seq { for mapping in resp.Split('&') do
+            let kv = mapping.Split('=')
+            yield kv.[0], kv.[1]}
+    |> Map.ofSeq
+ 
+/// Compute the 'Authorization' header for the given request data
+let authHeaderAfterAuthenticated url httpMethod token tokenSecret queryParams =
+    let signingKey = compositeSigningKey consumerSecret tokenSecret
+ 
+    let queryParameters =
+            ["oauth_consumer_key", consumerKey;
+             "oauth_nonce", nonce;
+             "oauth_signature_method", "HMAC-SHA1";
+             "oauth_token", token;
+             "oauth_timestamp", currentUnixTime.ToString();
+             "oauth_version", "1.0"]
+ 
+    let signingQueryParameters =
+        List.append queryParameters queryParams
+ 
+    let signingString = baseString httpMethod url signingQueryParameters
+    let oauth_signature = hmacsha1 signingKey signingString
+    let realQueryParameters = ("oauth_signature", oauth_signature)::queryParameters
+    let headerValue = createAuthorizeHeader realQueryParameters
+    headerValue
+ 
+let genericQuery token tokenSecret flickrMethod methodParams =
+    let signingKey = compositeSigningKey consumerSecret tokenSecret
+ 
+    let queryParameters =
+        ["oauth_consumer_key", consumerKey;
+         "oauth_nonce", nonce;
+         "oauth_signature_method", "HMAC-SHA1";
+         "oauth_token", token;
+         "oauth_timestamp", currentUnixTime.ToString();
+         "oauth_version", "1.0"]
+    let queryParametersMethod = ("method", flickrMethod)::queryParameters
+    let queryParametersUnsigned = match methodParams with
+                                  | Some x -> x::queryParametersMethod
+                                  | None -> queryParametersMethod 
+ 
+    let signingString = baseString "POST" accessTokenURI queryParametersUnsigned
+    let oauth_signature = hmacsha1 signingKey signingString
+   
+    let realQueryParameters = ("oauth_signature", oauth_signature)::queryParametersUnsigned
+   
+    let headerValue = createAuthorizeHeader realQueryParameters
+   
+    Http.RequestString( apiMethodCallsURI,
+                        query = realQueryParameters,
+                        httpMethod = "GET")
+    |> FlickrParser.ParseSets
+    
+let getAllSets token tokenSecret =
+    genericQuery token tokenSecret "flickr.photosets.getList" None
+
+[<EntryPoint>]
+let main args =
+    let auth =
+        if args.Length = 3 then
+            [   "oauth_token", args.[0];
+                "oauth_token_secret", args.[1];
+                "user_nsid", args.[2] ]
+            |> Map.ofList
+        else
+            let authres = requestToken()
+            let url = authorizeURI + "?oauth_token=" + authres.["oauth_token"]
+ 
+            System.Diagnostics.Process.Start("iexplore.exe", url) |> ignore
+            Console.WriteLine("Enter authorisation code: ")
+            let authcode = Console.ReadLine().Replace("-","").Replace(" ","")
+ 
+            Console.WriteLine("Authorisation code {0} used to get access token", authcode)
+ 
+            accessToken (authres.["oauth_token"]) (authres.["oauth_token_secret"]) (authcode.ToString())
+ 
+    printfn "Access Response: %A" auth
+ 
+    let sets = getAllSets (auth.["oauth_token"]) (auth.["oauth_token_secret"])
+ 
+    0
